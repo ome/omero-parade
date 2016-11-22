119c4b82c0430da5ca80efbb8b0c072475696365
William Moore <w.moore@dundee.ac.uk>
2016-11-22 23:07:05 +0000

    Add styles for icon/table layout buttons

--- src/js/IconTableHeader.js
+++ src/js/IconTableHeader.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 
-const styles = {
+let styles = {
     toolbar: {
         position: 'absolute',
         top: 0,
@@ -12,8 +12,43 @@ const styles = {
         borderBottom: 'solid 1px hsl(210,10%,90%)',
         overflow: 'hidden',
         background: 'none repeat scroll 0 0 #EFF1F4',
+    },
+    chooser: {
+        position: 'absolute',
+        right: 20,
+        top: 4,
+        border: '1px solid #9EA5AC',
+        backgroundColor: '#E3E5E7',
+        borderRadius: 4,
+        padding: 0,
+    },
+    button: {
+        width: 25,
+        height: 20,
+        borderRight: '1px solid #9EA5AC',
+        borderLeft: 0,
+        borderBottom: 0,
+        borderTop: 0,
+        cusor: 'pointer'
+    },
+    btnLeft: {
+        borderBottomLeftRadius: 4,
+        borderTopLeftRadius: 4,
+    },
+    btnRight: {
+        borderBottomRightRadius: 4,
+        borderTopRightRadius: 4,
+    },
+    checked: {
+        backgroundColor: '#B2BDCB',
     }
 }
+
+// We add the styles.button to btnLeft & btnRight styles
+Object.assign(styles.btnLeft, styles.button)
+Object.assign(styles.btnRight, styles.button, {borderRight: 0})
+
+const a = Object.assign
 
 const IconTableHeader = React.createClass({
 
@@ -30,21 +65,24 @@ const IconTableHeader = React.createClass({
     render: function() {
         var layout = this.props.layout,
             filterText = this.props.filterText;
-        var iconBtnClass = layout === "icon" ? "checked" : "",
-            tableBtnClass = layout === "table" ? "checked" : "";
+        // Add 'checked' style to appropriate button depending on layout
+        var iconBtnStyle = layout === "icon" ? a({}, styles.btnLeft, styles.checked) : styles.btnLeft,
+            tableBtnStyle = layout === "table" ? a({}, styles.btnRight, styles.checked) : styles.btnRight;
         return (
             <div style={styles.toolbar} >
-                <div id="layout_chooser">
+                <div style={styles.chooser} >
                     <button
                         onClick={this.handleLayoutClick}
                         title="View as Thumbnails"
                         data-layout="icon"
-                        className={iconBtnClass} />
+                        style={iconBtnStyle}
+                        className="parade_icon_layout" />
                     <button
                         onClick={this.handleLayoutClick}
                         title="View as List"
                         data-layout="table"
-                        className={tableBtnClass} />
+                        style={tableBtnStyle}
+                        className="parade_table_layout" />
                 </div>
                 <form className="search filtersearch" id="filtersearch" action="#" style={{top: 4}}>
                     <div>
