01e53b19c959a6ff9b24f363b64f8aa144d664ab
William Moore <w.moore@dundee.ac.uk>
2018-03-05 23:21:32 +0000

    Remove clustering and simplify sorting in DataTable

--- src/js/dataView/table/DataTable.js
+++ src/js/dataView/table/DataTable.js
@@ -18,7 +18,6 @@
 
 import React, { Component } from 'react';
 import { getHeatmapColor } from '../../util';
-import clusterfck from 'clusterfck';
 
 class DataTable extends React.Component {
 
@@ -30,8 +29,6 @@ class DataTable extends React.Component {
         }
         this.handleSortTable = this.handleSortTable.bind(this);
         this.handleShowHeatmap = this.handleShowHeatmap.bind(this);
-        this.clusterTableData = this.clusterTableData.bind(this);
-        this.traverseCluster = this.traverseCluster.bind(this);
     }
 
     componentDidMount() {
@@ -72,76 +69,19 @@ class DataTable extends React.Component {
         });
     }
 
-    clusterTableData() {
-        // make a list of data for each image from state.tableData
-        // Each item of tableData is already a dict of {imageId: value}
-        let dataKeys = Object.keys(this.props.tableData);
-        if (dataKeys.length < 2) return;
-        // make dict of {imageId: [v1, v2, v3]}.
-        let imageIds = this.props.imgJson.map(i => i.id);
-        let toCluster = imageIds.map(iid => {
-            return dataKeys.map(key => this.props.tableData[key][iid]);
-        });
-        // Make a lookup of tableDataKey :image_id so that when we get clustered tableDAta
-        // we can work out which image each 'row' of data came from (since we can't
-        // include image ID in the cluster data)
-        // We know the 'toCluster' list is in same order as imageIDs just now...
-        let clusterLookup = imageIds.reduce((prev, iid, index) => {
-            let dataString = toCluster[index].join(",");
-            prev[dataString] = iid;
-            return prev;
-        }, {});
-        // let toCluster = [[255, 255, 240],
-        //     [20, 120, 102],
-        //     [250, 255, 253],
-        //     [100, 54, 300]];
-        let threshold = 25000;
-        var clusters = clusterfck.hcluster(toCluster, clusterfck.EUCLIDEAN_DISTANCE,
-            clusterfck.AVERAGE_LINKAGE, threshold);
-        let orderedResults = this.traverseCluster(clusters);
-
-        let orderedImageIds = orderedResults.map(res => clusterLookup[res.join(",")]);
-        return orderedImageIds;
-    }
-
-    traverseCluster(clusters) {
-
-        let out = [];
-        function traverseNode(node) {
-            if (node.left && node.right) {
-                traverseNode(node.left);
-                traverseNode(node.right);
-            } else {
-                out.push(node.value);
-            }
-        }
-        clusters.forEach(n => {traverseNode(n)});
-        return out;
-    }
-
     render() {
         let {imgJson, iconSize, tableData, selectedWellIds} = this.props;
         if (this.state.sortBy != undefined) {
-            let orderedImageIds;
-            if (this.state.sortBy === "cluster") {
-                orderedImageIds = this.clusterTableData();
-            } else {
-                let colDataToSort = this.props.tableData[this.state.sortBy];
-                // convert {iid: value} to [value, iid]
-                let dataList = [];
-                for (var iid in colDataToSort) {dataList.push([iid, colDataToSort[iid]])};
-                dataList.sort((a, b) => a[1] < b[1] ? -1 : 1);
-                orderedImageIds = dataList.map(l => l[0]);
-            }
-            if (orderedImageIds) {
-                // sort filteredImages by ID...
-                // create lookup {iid: image}
-                let imgLookup = imgJson.reduce((prev, img) => {
-                    prev[img.id] = img;
-                    return prev;
-                }, {});
-                imgJson = orderedImageIds.map(iid => imgLookup[iid]);
-            }
+            // let orderedImageIds;
+            let colDataToSort = this.props.tableData[this.state.sortBy];
+            // Add a sortKey to imgJson
+            imgJson = imgJson.map(i => Object.assign(i, {sortKey: colDataToSort[i.id]}));
+            // sort...
+            imgJson.sort((a, b) => {
+                if (a.sortKey === undefined) return -1;
+                if (b.sortKey === undefined) return 1;
+                return a.sortKey < b.sortKey ? -1 : 1;
+            });
         }
 
         let columnNames = Object.keys(tableData);
@@ -164,9 +104,6 @@ class DataTable extends React.Component {
                     <tbody>
                     <tr>
                         <td>
-                            <a onClick={(event) => {this.handleSortTable(event, "cluster")}}>
-                                Cluster
-                            </a>
                         </td>
                         <td>Name</td>
                         {columnNames.map(name => (
