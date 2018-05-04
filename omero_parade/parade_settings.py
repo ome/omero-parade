000ff3d1b4af69b8178fbc0d687ba349bb225555
Chris Allan <callan@glencoesoftware.com>
2018-05-04 11:43:38 +0100

    Move built-in filters out of the root module namespace

--- omero_parade/parade_settings.py
+++ omero_parade/parade_settings.py
@@ -26,7 +26,8 @@ PARADE_SETTINGS_MAPPING = {
 
     "omero.web.parade.filters":
         ["PARADE_FILTERS",
-         '["omero_parade", "annotation_filters", "table_filters"]',
+         '["omero_parade", "omero_parade.annotation_filters", '
+         '"omero_parade.table_filters"]',
          json.loads,
          ("Filters for filtering data. Each is a python module \
           that contains an omero_filter module"
