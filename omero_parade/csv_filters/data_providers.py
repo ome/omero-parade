922a34bb8777d6aabcf4ae8ace69efe731abbf95
William Moore <w.moore@dundee.ac.uk>
2020-02-24 13:07:30 +0000

    Fix copyright year to 2020

--- omero_parade/csv_filters/data_providers.py
+++ omero_parade/csv_filters/data_providers.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018 University of Dundee.
+# Copyright (c) 2020 University of Dundee.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
